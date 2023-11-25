0a64c601f44bcd9c4cef7fcf1dd823a087b7e161
Natyy__ <nataly.schmidt@escola.pr.gov.br>
2023-11-24 22:54:20 -0300

    adcionando paciente

--- js/index.js
+++ js/index.js
@@ -2,3 +2,14 @@
 let titulo = document.querySelector('.titulo');
 
 titulo.textContent = "Em forma!"
+
+let lista = document.querySelectorAll('.info-nome');
+let paciente =[];
+
+for(i = 0 ; i< lista.length ; i++){
+    
+ paciente.push(lista[i].textContent)
+}
+
+console.log(paciente)
+
